c7a8d985a98236b18653adeb5eb081912578d7d1
Andrew <xeroxandrew@gmail.com>
2025-04-12 05:48:19 -0400

    finished v1 of website

--- scripts/images.js
+++ scripts/images.js
@@ -1,9 +1,12 @@
-function openModal(src, title, link) {
+function openModal(src, link, linkText) {
     const modal = document.getElementById("imgModal");
     const modalImage = document.getElementById("modalImage");
-  
+    const modalLink = document.getElementById("modalLink");
+
     modal.style.display = "block";
     modalImage.src = src;
+    modalLink.href = link;
+    modalLink.textContent = linkText;
   }
   
   function closeModal() {
